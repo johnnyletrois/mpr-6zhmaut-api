82b372c06cf6e4f7d3dd5bea9e8c681b8f5234b5
johnnyletrois <jrmartin.iii@gmail.com>
2017-06-09 10:41:09 -0700

    Add files via upload

--- README.md
+++ README.md
@@ -1,2 +1,166 @@
 # mpr-6zhmaut-api
-Playing around with jnewland's monoprice six zone controller API
+
+Monoprice sells this wacky [6 zone amp](https://www.monoprice.com/product?p_id=10761) with a serial interface. This tiny project wraps the serial interface with a JSON API.
+
+## Installation
+
+    npm install
+
+## Running the server
+
+    PORT=8181 npm start
+
+### API
+
+#### GET /zones
+
+```js
+[
+  {
+    "zone": "11",
+    "pa": "00",
+    "pr": "01",
+    "mu": "00",
+    "dt": "00",
+    "vo": "15",
+    "tr": "10",
+    "bs": "10",
+    "bl": "10",
+    "ch": "01",
+    "ls": "00"
+  },
+  {
+    "zone": "12",
+    "pa": "00",
+    "pr": "01",
+    "mu": "00",
+    "dt": "00",
+    "vo": "15",
+    "tr": "10",
+    "bs": "10",
+    "bl": "10",
+    "ch": "01",
+    "ls": "00"
+  },
+  {
+    "zone": "13",
+    "pa": "00",
+    "pr": "01",
+    "mu": "00",
+    "dt": "00",
+    "vo": "15",
+    "tr": "10",
+    "bs": "10",
+    "bl": "10",
+    "ch": "01",
+    "ls": "00"
+  },
+  {
+    "zone": "14",
+    "pa": "00",
+    "pr": "01",
+    "mu": "00",
+    "dt": "00",
+    "vo": "10",
+    "tr": "10",
+    "bs": "12",
+    "bl": "10",
+    "ch": "02",
+    "ls": "00"
+  },
+  {
+    "zone": "15",
+    "pa": "00",
+    "pr": "01",
+    "mu": "00",
+    "dt": "00",
+    "vo": "20",
+    "tr": "07",
+    "bs": "07",
+    "bl": "10",
+    "ch": "01",
+    "ls": "00"
+  },
+  {
+    "zone": "16",
+    "pa": "00",
+    "pr": "01",
+    "mu": "00",
+    "dt": "00",
+    "vo": "21",
+    "tr": "07",
+    "bs": "07",
+    "bl": "10",
+    "ch": "01",
+    "ls": "00"
+  }
+]
+```
+
+#### GET /zones/:zone
+
+Returns the current status of a specific zone.
+
+```js
+{
+  "zone": "11",
+  "pa": "00",
+  "pr": "01",
+  "mu": "00",
+  "dt": "00",
+  "vo": "15",
+  "tr": "10",
+  "bs": "10",
+  "bl": "10",
+  "ch": "01",
+  "ls": "00"
+}
+```
+
+#### GET /zones/:zone/:attribute
+
+Return the value of a specific attribute in a zone as plain text.
+Valid attributes are:
+
+    pa (send zone 1 to all outputs)
+    power / pr
+    mute / mu
+    dt (??)
+    volume / vo
+    treble / tr
+    bass / bs
+    balance / bl
+    channel / source / ch
+    keypad status / ls
+
+    $ curl 192.168.1.254:8181/zones/11/bass
+    10
+
+#### POST /zones/:zone/:attribute
+
+Update a zone's attribute using the plain-text value of the body.
+
+    pa (send zone 1 to all outputs)
+    power / pr
+    mute / mu
+    dt (??)
+    volume / vo
+    treble / tr
+    bass / bs
+    balance / bl
+    channel / source / ch
+
+Here's an example where we turn the bass of zone 11 up:
+
+```
+$ curl 192.168.1.254:8181/zones/11
+{"zone":"11","pa":"00","pr":"01","mu":"00","dt":"00","vo":"15","tr":"10","bs":"10","bl":"10","ch":"01","ls":"00"}
+$ curl -X POST -d '12' 192.168.1.254:8181/zones/11/bass
+{"zone":"11","pa":"00","pr":"01","mu":"00","dt":"00","vo":"15","tr":"10","bs":"12","bl":"10","ch":"01","ls":"00"}
+```
+
+### Contributions
+
+* fork
+* create a feature branch
+* open a Pull Request
---
+++ app.js
@@ -0,0 +1,186 @@
+var express    = require("express");
+var morgan     = require("morgan");
+var bodyParser = require("body-parser");
+var serialport = require("serialport");
+var async      = require("async");
+
+var app = express();
+var logFormat = "'[:date[iso]] - :remote-addr - :method :url :status :response-time ms - :res[content-length]b'";
+app.use(morgan(logFormat));
+app.use(bodyParser.text({type: '*/*'}));
+
+var SerialPort = serialport.SerialPort;
+var device     = process.env.DEVICE || "/dev/ttyUSB0";
+var connection = new SerialPort(device, {
+  baudrate: 9600,
+  parser: serialport.parsers.readline("\n")
+});
+
+connection.on("open", function () {
+  var zones = {};
+
+  connection.write("?10\r");
+  connection.write("\r");
+  connection.write("?20\r");
+  connection.write("\r");
+  connection.write("?30\r");
+
+  connection.on('data', function(data) {
+    console.log(data);
+    var zone = data.match(/#>(\d{2})(\d{2})(\d{2})(\d{2})(\d{2})(\d{2})(\d{2})(\d{2})(\d{2})(\d{2})(\d{2})/);
+    if (zone != null) {
+      zones[zone[1]] = {
+        "zone": zone[1],
+        "pa": zone[2],
+        "pr": zone[3],
+        "mu": zone[4],
+        "dt": zone[5],
+        "vo": zone[6],
+        "tr": zone[7],
+        "bs": zone[8],
+        "bl": zone[9],
+        "ch": zone[10],
+        "ls": zone[11]
+      };
+    }
+  });
+
+  app.get('/zones', function(req, res) {
+    async.until(
+      function () { return typeof zones !== "undefined"; },
+      function (callback) {
+        setTimeout(callback, 10);
+      },
+      function () {
+        var zoneArray = new Array;
+        for(var o in zones) {
+          zoneArray.push(zones[o]);
+        }
+        res.json(zoneArray);
+      }
+    );
+  });
+
+  // Only allow query and control of single zones
+  app.param('zone', function(req, res, next, zone) {
+    if (zone % 10 > 0 && Number(zone) != "NaN") {
+      req.zone = zone;
+      next();
+    } else {
+      res.status(500).send({ error: zone + ' is not a valid zone'});
+    }
+  });
+
+  app.get('/zones/:zone', function(req, res) {
+    async.until(
+      function () { return typeof zones[req.zone] !== "undefined"; },
+      function (callback) {
+        setTimeout(callback, 10);
+      },
+      function () {
+        res.json(zones[req.zone]);
+      }
+    );
+  });
+
+  // Validate and standarize control attributes
+  app.param('attribute', function(req, res, next, attribute) {
+    if (typeof attribute !== 'string') {
+      res.status(500).send({ error: attribute + ' is not a valid zone control attribute'});
+    }
+    switch(attribute.toLowerCase()) {
+      case "pa":
+        req.attribute = "pa";
+        next();
+        break;
+      case "pr":
+      case "power":
+        req.attribute = "pr";
+        next();
+        break;
+      case "mu":
+      case "mute":
+        req.attribute = "mu";
+        next();
+        break;
+      case "dt":
+        req.attribute = "dt";
+        next();
+        break;
+      case "vo":
+      case "volume":
+        req.attribute = "vo";
+        next();
+        break;
+      case "tr":
+      case "treble":
+        req.attribute = "tr";
+        next();
+        break;
+      case "bs":
+      case "bass":
+        req.attribute = "bs";
+        next();
+        break;
+      case "bl":
+      case "balance":
+        req.attribute = "bl";
+        next();
+        break;
+      case "ch":
+      case "channel":
+      case "source":
+        req.attribute = "ch";
+        next();
+        break;
+      case "ls":
+      case "keypad":
+        req.attribute = "ls";
+        next();
+        break;
+      default:
+        res.status(500).send({ error: attribute + ' is not a valid zone control attribute'});
+    }
+  });
+
+  app.post('/zones/:zone/:attribute', function(req, res) {
+    zones[req.zone] = undefined;
+    connection.write("<"+req.zone+req.attribute+req.body+"\r");
+    connection.write("?10\r");
+    connection.write("\r");
+    connection.write("?20\r");
+    connection.write("\r");
+    connection.write("?30\r");
+    async.until(
+      function () { return typeof zones[req.zone] !== "undefined"; },
+      function (callback) {
+        setTimeout(callback, 10);
+      },
+      function () {
+        res.json(zones[req.zone]);
+      }
+    );
+  });
+
+  app.get('/zones/:zone/:attribute', function(req, res) {
+    zones[req.zone] = undefined;
+    connection.write("?10\r");
+    connection.write("\r");
+    connection.write("?20\r");
+    connection.write("\r");
+    connection.write("?30\r");
+    async.until(
+      function () { return typeof zones[req.zone] !== "undefined"; },
+      function (callback) {
+        setTimeout(callback, 10);
+      },
+      function () {
+        res.send(zones[req.zone][req.attribute]);
+      }
+    );
+  });
+
+  app.listen(process.env.PORT || 8181);
+});
+
+
